88cead8108ecfb597e8cd7dc119910474c023743
Brigitte Warner <brigitte.warner@gmail.com>
2014-09-09 18:02:43 -0400

    set up bootstrap-sass test suite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapTestRails::Application.config.secret_token = '07a2ae12630d70531c5a25b91ec1c723b9973756e67e06054f513e04331ff0d060bd13ec4536674c1e6376d9e5b0c8584dae32a9e4c46d1969da33e1651481f8'
